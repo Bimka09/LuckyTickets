86a5c4d6a34c5b4aff875568f852e0de07e3bd56
Bimka09 <90Alex09@mail.ru>
2022-05-17 20:51:58 +0300

    Добавьте файлы проекта.

---
+++ LuckyTickets/LuckyTicket.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace LuckyTickets
+{
+    public class LuckyTicket
+    {
+        private int _rank;
+        private int _currentRank;
+        private int[,] _data;
+        private int[] _sumsRow;
+
+        public void Start()
+        {
+            if(_currentRank == _rank)
+            {
+                Console.WriteLine(GetLuckyCount());
+            }
+            else
+            {
+                _currentRank += 2;
+                EditMatrix();
+                GetSumsRow();
+                Start();
+            }
+        }
+        public LuckyTicket(int rank)
+        {
+            _sumsRow = new int[1] { 1 };
+            _rank = rank;
+            _currentRank = 0;
+        }
+        private void EditMatrix()
+        {
+            int nullCell = -1;
+            _data = new int[10, 9 * _currentRank / 2 + 1];
+            for (int i = 0; i < _data.GetUpperBound(0) + 1; i++)
+            {
+                var index = 0;
+                for (int j = 0; j < _data.GetUpperBound(1) + 1; j++)
+                {
+                    if(nullCell >= j | index >= _sumsRow.Length)
+                    {
+                        _data[i, j] = 0;
+                    }
+                    else
+                    {
+                        _data[i, j] = _sumsRow[index];
+                        index++;    
+                    }
+                }
+                nullCell += 1;
+            }
+            _sumsRow = new int[9 * _currentRank / 2 + 1];
+
+        }
+        private void GetSumsRow()
+        {
+            for (int i = 0; i < _data.GetUpperBound(1) + 1; i++)
+            {
+                int sumCol = 0;
+                for (int j = 0; j < _data.GetUpperBound(0) + 1; j++)
+                {
+                    sumCol += _data[j,i];
+                }
+                _sumsRow[i] = sumCol;
+            } 
+        }
+        private long GetLuckyCount()
+        {
+            long luckyCount = 0;
+            for (int i = 0; i < _sumsRow.Length; i++)
+            {
+                luckyCount += (long)Math.Pow(_sumsRow[i], 2);
+            }
+            return luckyCount;
+        }
+    }
+
+}
---
+++ LuckyTickets/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LuckyTickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 2; i <= 20; i += 2)
+            {
+                var lacky = new LuckyTicket(i);
+                lacky.Start();
+            }
+             
+        }
+    }
+}
